9fc898441af91aabf3e6a1faa5df81e35e642f5a
KEVIN <karimboyevshaxram97@gmail.com>
2026-02-12 16:22:56 +0900

    BRR: react loyixa

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
+
+function App() {
+  return <Container maxWidth="sm">
+    <Stack flexDirection={"column"}>
+    <Box sx={{my: 4}}>
+      <Typography variant="h4" component={"h4"}>
+        Welcome to the Home Page
+      </Typography>
+    </Box>
+    <Box>
+      <RippleBadge badgeContent={4}>
+        <Button variant="contained">Notifications</Button>
+      </RippleBadge>
+    </Box>
+    </Stack>
+  </Container>;
+}
+
+export default App;
+
+
+
+

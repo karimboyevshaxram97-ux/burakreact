bed3fea136f3ec51f53a7dbbcd5d6b09272bb273
KEVIN <karimboyevshaxram97@gmail.com>
2026-02-17 02:25:39 +0900

    fix: homeNavbar menu part refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ root.render(
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  
 );
 
 // If you want to start measuring performance in your app, pass a function

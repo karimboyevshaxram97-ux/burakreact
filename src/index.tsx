5e194bd18fdf4b87ec972081e1425d128558b945
KEVIN <karimboyevshaxram97@gmail.com>
2026-02-17 01:48:39 +0900

    fix: modifiy root index render logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
@@ -11,8 +11,10 @@ import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>

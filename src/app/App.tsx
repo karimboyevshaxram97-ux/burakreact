59bd335610f982ce4920747ddd2b7f9b70f21e13
KEVIN <karimboyevshaxram97@gmail.com>
2026-03-19 04:57:57 +0900

    feat publish authentication modal companent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,11 +14,18 @@ import "../css/footer.css";
 import "../css/home.css";
 import { CartItem } from "../lib/types/search";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./companents/auth";
 
 function App() {
   const location = useLocation();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll} = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
 
+  /** HANDLERS **/
+
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -58,6 +65,14 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+  
+
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
+       />
     </>
   );
 }
